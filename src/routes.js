304884b1710b968e31af3784c5036cbe4848e390
jiandaLi <ljdlee@outlook.com>
2017-04-21 16:16:34 +0800

    video

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,7 @@
 import React from 'react' // 引入react
 import { Route, IndexRoute } from 'react-router' // 引入react路由
 import { Home, App, Player, Question, QuestionList, Select, SideBar,Header,rightAns,Chart,Dashboard,ViewQuestion,Syllabus,Example,LectureNotes,ExampleList,Exercise,ExerciseList,Problem,ProblemList
-,Diy,DiyList} from './containers' // 引入各容器组件
+,Diy,DiyList,Video} from './containers' // 引入各容器组件
 
 export default (
     <Route path="/" component={App}>
@@ -15,6 +15,7 @@ export default (
         <Route path="Dashboard" component={Dashboard}/>
         <Route path="ViewQuestion" component={ViewQuestion}/>
         <Route path="Syllabus" component={Syllabus}/>
+        <Route path="Video" component={Video}/>
         <Route path="Example" component={Example}/>
         <Route path="LectureNotes" component={LectureNotes}/>
         <Route path="ExampleList" component={ExampleList}/>
